225c7e31a71adf0da253b692d43356782c516d8a
Menendez NELSON <menendezon@gmail.com>
2023-05-27 15:45:23 +0000

    Update functions.test.js

--- functions.test.js
+++ functions.test.js
@@ -1,1 +1,24 @@
+const { stringLength } = require('./functions');
 
+describe('stringLength', () => {
+  it('should return the correct length of a string', () => {
+    expect(stringLength('Hello')).toBe(5);
+    expect(stringLength('')).toBe(0);
+    expect(stringLength('1234567890')).toBe(10);
+  });
+
+  it('should throw an error if the input is not a string', () => {
+    expect(() => stringLength(123)).toThrow('Input must be a string');
+    expect(() => stringLength(null)).toThrow('Input must be a string');
+    expect(() => stringLength(undefined)).toThrow('Input must be a string');
+    expect(() => stringLength({})).toThrow('Input must be a string');
+  });
+
+  it('should throw an error if the string length is less than 1', () => {
+    expect(() => stringLength('')).toThrow('String length must be between 1 and 10 characters');
+  });
+
+  it('should throw an error if the string length is greater than 10', () => {
+    expect(() => stringLength('This string is too long')).toThrow('String length must be between 1 and 10 characters');
+  });
+});
